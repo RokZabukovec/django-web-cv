7dd1f7a9c52a9a759505fd5df50a706e140645d9
Rok Zabukovec <kzabuk@gmail.com>
2018-10-28 19:47:02 +0100

    added pdf cv download

--- personal/urls.py
+++ personal/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-from . import views
-
 urlpatterns = [
+    url(r'download$', views.download_pdf,),
     url(r'^$', views.index, name='index'),
 ]
--- personal/views.py
+++ personal/views.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-
+from django.http import FileResponse, Http404
+import os
 
 def index(request):
     contex = {}
     return render(request, "index.html", contex)
+
+
+def download_pdf(request):
+    try:
+        script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+        rel_path = "media/mypdf.pdf"
+        abs_file_path = os.path.join(script_dir, rel_path)
+        return FileResponse(open(abs_file_path, 'rb'), content_type='application/pdf')
+    except OSError:
+        raise Http404()
